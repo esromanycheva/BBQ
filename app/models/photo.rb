da4720d1b84810a8d314f2ce5bf987e6c1a2aaff
esromanycheva <esromanycheva@gmail.com>
2022-05-30 21:59:20 +0300

    photo and active storage

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -5,9 +5,7 @@ class Photo < ApplicationRecord
   validates :event, presence: true
   validates :user, presence: true
 
-  # has_one_attached :photo
-
-  mount_uploader :photo, PhotoUploader
+  has_one_attached :photo
 
   scope :persisted, -> { where "id IS NOT NULL" }
 end
