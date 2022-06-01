9461bbb44fad98f3d879dda4b391b2ef10743152
esromanycheva <esromanycheva@gmail.com>
2022-06-01 09:04:45 +0300

    event-mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,13 +71,14 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
   config.action_mailer.delivery_method = :smtp
-
   config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: '587',
-    user_name: 'kateromanych', # не используйте для тестов свои реальные ящики
-    password: 'bolds1986', # не храните здесь пароль!
+    address: "smtp.gmail.com",
+    domain: "gmail.com",
+    port: 587,
+    user_name: "kateromanych",
+    password: "bolds1986",
     authentication: 'plain',
     enable_starttls_auto: true
   }
+
 end
